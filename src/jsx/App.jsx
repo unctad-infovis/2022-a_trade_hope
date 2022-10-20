e720a342b9887c1fb1b995e15bab9658e37a8a22
Teemo Tebest <teemo.tebest@gmail.com>
2022-10-20 21:10:00 +0200

    Updated styles

--- src/jsx/App.jsx
+++ src/jsx/App.jsx
@@ -31,10 +31,15 @@ function App() {
           <h3 className="subtitle highlight">The Role Of The Black Sea Grain Initiative</h3>
           <h3 className="subtitle">In Bringing Ukrainian Grain To The World</h3>
           <h4 className="date">20 October 2022</h4>
-          <p className="button_container"><a href="https://unctad.org/webflyer/trade-hope-role-black-sea-grain-initiative-bringing-ukrainian-grain-world" className="button">Download the report in PDF</a></p>
         </div>
+        <svg className="arrows">
+          <path className="a1" d="M0 0 L30 32 L60 0" />
+          <path className="a2" d="M0 20 L30 52 L60 20" />
+          <path className="a3" d="M0 40 L30 72 L60 40" />
+        </svg>
         <div className="ship_container" />
       </div>
+      <p className="button_container"><a href="https://unctad.org/webflyer/trade-hope-role-black-sea-grain-initiative-bringing-ukrainian-grain-world" className="button">Download the report in PDF</a></p>
       {
       // Quote container
       }
